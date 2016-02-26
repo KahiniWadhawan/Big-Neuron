4b2af4b1344de30d3a1515b6f28219c8ce5913fe
Kahini Wadhawan <kahiniwadhwan@gmail.com>
2016-02-26 01:13:21 -0700

    The Tweepy API and Twitter API in general only allow a limited number of calls to retrieve information. Added code to try to retrieve tweets using Tweepy API and insert them in the MongoDB database. After the limit is reached, added partial handling to try again and retrieve the remaining tweets for the user. Some refactoring and code cleanup needed.

--- preprocessing/pull_tweets.py
+++ preprocessing/pull_tweets.py
@@ -40,3 +40,57 @@ user = api.get_user('realDonaldTrump')
 #    # The id /id_str is the unique identifier for a tweet. We'll use id_str as it's unicode
 #    print tweet.id_str
 #    print "------------------"
+
+#print len(tweets)
+#-------------------------------------------------------------------
+# Insert all tweets returned by the Cursor API and store them in MongoDB tweets collection
+# P.S. To access the json for a tweet object, just call ._json on the tweet object
+#-------------------------------------------------------------------
+
+#recent_tweet = collection.find().sort([("_id", pymongo.DESCENDING)]).limit(1)
+#print recent_tweet.__class__
+#print recent_tweet
+#j = recent_tweet.next()
+#print j['id_str']
+#
+#exit()
+
+
+#-------------------------------------------------------------------
+# First insert the items from the first request in MongoDB collection
+#-------------------------------------------------------------------
+#global tweets
+tweets = None
+if (collection.count() == 0 ):
+    print "collection count is 0"
+    tweets = tweepy.Cursor(api.user_timeline, screen_name='realDonaldTrump').items()
+    # tweets is an 'tweepy.cursor.ItemIterator' object
+    for tweet in tweets:
+        #print "inserting tweets in collection"
+        collection.insert_one(tweet._json)
+
+while tweets:
+    #print "reiterating tweets for looking recent tweets"
+    recent_tweet = collection.find().sort([("_id", pymongo.DESCENDING)]).limit(1)
+    # recent_tweet is an 'pymongo.cursor.Cursor' object.
+    # Use count() to check the cursor actually points to an object or not
+    if (recent_tweet.count() > 0 ):
+        #print "recent tweets count 0"
+        recent_tweet = recent_tweet.next()
+        recent_id = recent_tweet['id_str']
+    else:
+        recent_id = 0
+    #global tweets
+    tweets = tweepy.Cursor(api.user_timeline, screen_name='realDonaldTrump', since_id=recent_id).items()
+    # 'tweepy.cursor.ItemIterator' object has num_objects field which returns
+    # the number of tweets returned by calling the API above
+    if tweets.num_tweets == 0 :
+        print "Waiting for 15 mins. Will request Twitter again"
+        time.sleep(60*15) # Wait for 15 mins
+        pass
+    else:
+        # Got new data from Twitter
+        # Add the newly found tweets in MongoDB
+        for tweet in tweets:
+            collection.insert_one(tweet._json)
+
