21aaafd4bbac974b7190b8c925efa493816338aa
Tanvi Parikh <tapa8728@colorado.edu>
2016-03-06 03:04:44 -0700

    Json(for 1 tweet) from IBM Tone Analyzer added

--- analytics/sentimentAnalysis/IBM Tone Analyzer/analyze_tone.py
+++ analytics/sentimentAnalysis/IBM Tone Analyzer/analyze_tone.py
@@ -2,8 +2,8 @@
 import json
 from watson_developer_cloud import ToneAnalyzerV3Beta as ToneAnalyzer
 
-tone_analyzer = ToneAnalyzer(username=service_username,
-                             password=service_password,
+tone_analyzer = ToneAnalyzer(username='f7c9ae7a-a947-41b0-81e9-8ff1ae0e6084',
+                             password='zLbkmbouGY8m',
                              version='2016-02-11')
 
 text = "The IBM Watson™ Tone Analyzer Service uses linguistic analysis to detect three types of tones from written text emotions, social tendencies, and writing style. Emotions identified include things like anger, cheerfulness and sadness. Identified social tendencies include things from the Big Five personality traits used by some psychologists."
