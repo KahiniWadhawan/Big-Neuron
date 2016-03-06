d4f39a0479e8109ca808a6c386001a57d9a4b255
Kahini Wadhawan <kahiniwadhwan@gmail.com>
2016-03-06 00:09:39 -0700

    adding directory iterator and corpus class

--- analytics/topics/vectorize_docs.py
+++ analytics/topics/vectorize_docs.py
@@ -28,3 +28,34 @@ stoplist = set(nltk.corpus.stopwords.words("english"))
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
+#--------------------------------------------------------------------
+# Text pre-processing and creation of bag of words format
+#--------------------------------------------------------------------
+def iter_docs(topdir, stoplist):
+    for fn in os.listdir(topdir):
+        if fn.__eq__('tweets.txt'):
+            fin = open(os.path.join(topdir, fn), 'rb')
+            text = fin.read()
+            texts = text.split("----------------------")[:-1]
+
+            #iterating over texts list - every text = one tweet - treated as one doc
+            for text in texts:
+                yield (x for x in
+                        gensim.utils.tokenize(text, lowercase=True, deacc=True,
+                                        errors="ignore")
+                        if x not in stoplist)
+
+            fin.close()
+
+
+class MyCorpus(object):
+
+        def __init__(self, topdir, stoplist):
+            self.topdir = topdir
+            self.stoplist = stoplist
+            self.dictionary = gensim.corpora.Dictionary(iter_docs(topdir, stoplist))
+
+        def __iter__(self):
+            for tokens in iter_docs(self.topdir, self.stoplist):
+                yield self.dictionary.doc2bow(tokens)
+
