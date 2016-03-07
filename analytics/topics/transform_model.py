f0ba3c7ae38ed11f99cba1f6f7fc81c66a8ad274
Kahini Wadhawan <kahiniwadhwan@gmail.com>
2016-03-06 20:48:07 -0700

    adding event logging

--- analytics/topics/transform_model.py
+++ analytics/topics/transform_model.py
@@ -1,3 +1,15 @@
-#-----------------------------------------------------------------
+#---------------------------------------------------------------------------
 #Author: Kahini Wadhawan
-#-----------------------------------------------------------------
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+#This file creates features from bow corpus. It applies TF-IDF, LSI and LDA
+# to find topics.
+#---------------------------------------------------------------------------
+
+import gensim
+import logging
+import os
+from gensim import corpora, models
+
+# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+#                     level=logging.INFO)
