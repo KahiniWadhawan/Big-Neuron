70b83bfd97a2ce82458b0def75ab9ebf747f6fda
Kahini Wadhawan <kahiniwadhwan@gmail.com>
2016-03-08 15:59:32 -0700

    created function and converted json str to json object

--- analytics/sentimentAnalysis/analyze_tone.py
+++ analytics/sentimentAnalysis/analyze_tone.py
@@ -1,11 +1,28 @@
+#-------------------------------------------------------------------------------
+#Author: Kahini Wadhawan
+#------------------------------------------------------------------------------
 # -*- coding: utf-8 -*-
 import json
 from watson_developer_cloud import ToneAnalyzerV3Beta as ToneAnalyzer
 
-tone_analyzer = ToneAnalyzer(username='service-username',
-                             password='service-password',
-                             version='2016-02-11')
+def get_sentiment(text):
+    #create Bluemix account to get service_username and service_password
+    service_username = "fill--your---service--username"
+    service_password ="fill--your---service---password"
 
-text = "The IBM Watson™ Tone Analyzer Service uses linguistic analysis to detect three types of tones from written text emotions, social tendencies, and writing style. Emotions identified include things like anger, cheerfulness and sadness. Identified social tendencies include things from the Big Five personality traits used by some psychologists."
+    tone_analyzer = ToneAnalyzer(username=service_username,
+                                 password=service_password,
+                                 version='2016-02-11')
 
-print(json.dumps(tone_analyzer.tone(text=text), indent=2))
+    sentiment_json = json.dumps(tone_analyzer.tone(text=text), indent=2)
+    return sentiment_json
+
+
+# text = "The IBM Watson Tone Analyzer Service uses linguistic analysis " \
+#        "to detect three types of tones from written text emotions, social tendencies, " \
+#        "and writing style. Emotions identified include things like anger, cheerfulness" \
+#        "and sadness. Identified social tendencies " \
+#        "include things from the Big Five personality traits used by some psychologists."
+
+
+
