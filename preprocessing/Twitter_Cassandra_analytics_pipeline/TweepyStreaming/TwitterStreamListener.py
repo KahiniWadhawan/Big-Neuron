646fce503df796c6a0c19b0b2f6803af5bea42f8
Michael Muehlbradt <micha.muehlbradt@gmail.com>
2016-03-20 14:56:45 -0600

    added twitter streaming API for real time tweet streaming

---
+++ preprocessing/Twitter_Cassandra_analytics_pipeline/TweepyStreaming/TwitterStreamListener.py
@@ -0,0 +1,36 @@
+import tweepy
+import json
+import TOKENS
+
+
+# Authentication details. To  obtain these visit dev.twitter.com
+consumer_key = TOKENS.consumer_key
+consumer_secret = TOKENS.consumer_secret
+access_token = TOKENS.access_token
+access_token_secret = TOKENS.access_token_secret
+
+# This is the listener, resposible for receiving data
+class StdOutListener(tweepy.StreamListener):
+    def on_data(self, data):
+        # Twitter returns data in JSON format - we need to decode it first
+        decoded = json.loads(data)
+
+        # Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
+        print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
+        print ''
+        return True
+
+    def on_error(self, status):
+        print status
+
+if __name__ == '__main__':
+    l = StdOutListener()
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+
+    # There are different kinds of streams: public stream, user stream, multi-user streams
+    # In this example follow #programming tag
+    # For more details refer to https://dev.twitter.com/docs/streaming-apis
+    stream = tweepy.Stream(auth, l)
+    stream.filter(track=['Snarfblob', 'Harry Potter'])
