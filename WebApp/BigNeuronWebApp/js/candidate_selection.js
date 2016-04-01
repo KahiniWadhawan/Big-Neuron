bb7bae5f11b5200d0191670cced5e30961cc2582
Jessica Lynch <jessica.lynch@colorado.edu>
2016-04-01 01:04:00 -0600

    setup of JS to handle different candidate radio button selection on home page (final) -- went with separate .js

--- WebApp/BigNeuronWebApp/js/candidate_selection.js
+++ WebApp/BigNeuronWebApp/js/candidate_selection.js
@@ -2,27 +2,15 @@
  * Used to redirect to appropriate candidate pages based on particular
  * candidate radio button selected on app's home page.
  */
-function idForm(){
-    var selectvalue = $('input[name=optradio]:checked', '#idForm').val();
-
-    if(selectvalue === "r0"){
-    window.open('http://www.google.com','_self');
-    return true;
+function whichCandidate(form){
+    var candidates = form.elements.candidate;
+    var i          = candidates.length;
+    
+    while (--i > -1){
+        if(candidates[i].checked){
+            return candidates[i].value;
+        }
     }
-    else if(selectvalue === "r1"){
-    window.open('http://www.google.com','_self');
-    return true;
-    }else if(selectvalue === 'r2'){
-    window.open('http://www.google.com','_self');
-    return true;
-    }else if(selectvalue === 'r3'){
-    window.open('http://www.google.com','_self');
-    return true;
-    }else if(selectvalue === 'r4'){
-    window.open('http://www.google.com','_self');
-    return true;
-    }
-    return false;
-    };
+}
 
 
