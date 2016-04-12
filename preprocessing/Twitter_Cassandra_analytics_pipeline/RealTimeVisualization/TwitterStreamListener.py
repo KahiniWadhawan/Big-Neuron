59978b0e07381c908e6529c04e6e47b9d17c4744
Michael Muehlbradt <micha.muehlbradt@gmail.com>
2016-04-12 14:51:23 -0600

    edited twitter streaming to output JSon to file with filter function

--- preprocessing/Twitter_Cassandra_analytics_pipeline/RealTimeVisualization/TwitterStreamListener.py
+++ preprocessing/Twitter_Cassandra_analytics_pipeline/RealTimeVisualization/TwitterStreamListener.py
@@ -11,28 +11,17 @@ access_token_secret = TOKENS.access_token_secret
 
 # This is the listener, resposible for receiving data
 class StdOutListener(tweepy.StreamListener):
-	def on_data(self, data):
-		print data
-		with open("data.json", 'w') as fp:
-			fp.write(data)
-		# Twitter returns data in JSON format - we need to decode it first
-		decoded = json.loads(data)
+    def on_data(self, data):
+        # Twitter returns data in JSON format - we need to decode it first
+        decoded = json.loads(data)
 
-		# Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
-		print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
-		print ''
-		return True
+        # Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
+        print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
+        print ''
+        return True
 
-	def on_error(self, status):
-		print status
-        
-    #Stream filtering
-	def changeFilter(self, filterList):
-		if type(filterList) is not str:
-			print"Please enter a string"
-			raise TypeError
-		stream.filter(track=[filterList])
-		
+    def on_error(self, status):
+        print status
 
 if __name__ == '__main__':
     l = StdOutListener()
@@ -43,8 +32,5 @@ if __name__ == '__main__':
     # There are different kinds of streams: public stream, user stream, multi-user streams
     # In this example follow #programming tag
     # For more details refer to https://dev.twitter.com/docs/streaming-apis
-    
     stream = tweepy.Stream(auth, l)
-    filterList = 'Donald Trump'
-    l.changeFilter(filterList)
-    #stream.filter(track=['Snarfblob'])
+    stream.filter(track=['Snarfblob'])
