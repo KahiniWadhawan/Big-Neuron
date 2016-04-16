6580e9fe6e015f25b8cd8ab495acd3a3bdb79802
Jessica Lynch <jessica.lynch@colorado.edu>
2016-04-16 14:59:59 -0600

    testing (1)

---
+++ WebApp/flask-app/tweetlevel.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+# Fetch and visualize data for sentence level sentiment analysis
+
+__author__ = "Jessica"
+__date__ = "$Apr 16, 2016 2:46:14 PM$"
+
+import bigneuron_mongodb_api as bgdb
+
+data = bgdb.fetch_sentiment_json(20)
+
+
+
+
+
