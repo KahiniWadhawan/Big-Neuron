b96fc50046ce4f7529a2bd8a534191d5329e497d
Kahini Wadhawan <kahiniwadhwan@gmail.com>
2016-04-19 16:54:41 -0600

    added user functions and tweet fetching functions

--- databases/cassandra/bigneuron_cassandradb_api.py
+++ databases/cassandra/bigneuron_cassandradb_api.py
@@ -4,7 +4,13 @@
 #-----------------------------------------------------------
 
 import tweepy
+import json
+import time
+import os
+import sys
+
 from cassandra.cluster import Cluster
+
 
 #-----------------------------------------------------------
 #Loading configs for Tweepy
@@ -21,5 +27,34 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 #----------------------------------------------------------
 #Connecting to Cassandra DB Keyspace
 #----------------------------------------------------------
-cluster = Cluster()
-session = cluster.connect('twitterdataset')
+#revisit - add function to execute cassandra_setup.sh from here
+def db_connect():
+    cluster = Cluster()
+    session = cluster.connect('twitterdataset')
+    return session
+
+
+#---------------------------------------------------
+# Get the User object for Election Candidates
+#---------------------------------------------------
+def get_user(username):
+    #username = 'realDonaldTrump'
+    user = api.get_user(username)
+    return user
+
+def get_user_tweets(username,num=None):
+    #username = 'realDonaldTrump'
+    tweets = None
+    if not num:
+        tweets = tweepy.Cursor(api.user_timeline, screen_name=username).items()
+    else:
+        tweets = tweepy.Cursor(api.user_timeline, screen_name=username).items(num)
+
+    return tweets
+
+
+
+
+
+
+
