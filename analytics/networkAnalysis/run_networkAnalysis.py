b6394be5a59a869102df3896904a242e4adc9b91
Kahini Wadhawan <kahiniwadhwan@gmail.com>
2016-04-25 03:40:58 -0600

    main python file for running graph analytics [#118267467]

--- analytics/networkAnalysis/run_networkAnalysis.py
+++ analytics/networkAnalysis/run_networkAnalysis.py
@@ -7,7 +7,7 @@
 # and visulization for networks
 #---------------------------------------------------------------
 from gen_graph import get_edgelist, fill_out_graph
-#from analysis_functions import run_analysis
+from analysis import run_analysis
 import tweepy
 import json
 
@@ -27,3 +27,28 @@ auth = tweepy.OAuthHandler(oauth['consumer_key'], oauth['consumer_secret'])
 auth.set_access_token(oauth['access_token'], oauth['access_token_secret'])
 api = tweepy.API(auth)
 
+def main():
+    get_edgelist()
+    run_analysis()
+    fill_out_graph()
+
+def get_info():
+    settings = {}
+    settings['screen_name'] = raw_input('Who do you want to run this for?: ')
+    settings['size'] = int(raw_input('How many nodes?: '))
+
+    #api = pull_tweets.connect_twitter() -revisit
+    user_info = api.get_user(screen_name=settings['screen_name'])
+    settings['user_id'] = int(user_info.id)
+
+    f = open('user_info.json', 'wb')
+    f.seek(0)
+    settings_json = json.dumps(settings, sort_keys=True, indent=4)
+    f.write(settings_json)
+    f.truncate()
+    f.close()
+
+    main()
+
+if __name__ == '__main__':
+    get_info()
