6456b991835953a02f54baf6a846f30088ece08c
Kahini Wadhawan <kahiniwadhwan@gmail.com>
2016-04-25 03:37:45 -0600

    flask server file for graph analytics [#118267467]

---
+++ analytics/networkAnalysis/server.py
@@ -0,0 +1,17 @@
+#------------------------------------------------------
+#Author: Kahini Wadhawan
+#------------------------------------------------------
+import os
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('graph.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    #app.run(host='0.0.0.0', port=port)
+    app.run()
+
