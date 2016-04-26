a9a5403a88ae38b12ba26087fcc49e9fe3426333
Kahini Wadhawan <kahiniwadhwan@gmail.com>
2016-04-26 03:58:55 -0600

    merging working app.py with Sentence level changes

--- WebApp/flask-app/app.py
+++ WebApp/flask-app/app.py
@@ -1,9 +1,9 @@
-''' Authors - 
+''' Authors -
         Jessica Lynch
         Tanvi Parikh
-    Purpose - 
+    Purpose -
         Integration of Flask with Webapp
-    Date - 
+    Date -
         16th April, 2016
 '''
 
@@ -13,13 +13,18 @@ __date__ = "$Apr 14, 2016 11:39:45 PM$"
 from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
-
 # Sessions variables are stored client side, on the users browser
 # the content of the variables is encrypted, so users can't
 # actually see it. They could edit it, but again, as the content
 # wouldn't be signed with this hash key, it wouldn't be valid
-# You need to set a scret key (random text) and keep it secret
+# You need to set a secret key (random text) and keep it secret
 app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
+
+#sys.path.append(os.path.dirname(__file__) + r"/static/data/dummy_db.py")  #append path to db api module that has the method to get list of top tweets
+# from dummy_db import get_tweet_list, get_tweet_tones  #Do we have a tone analyzer module -or- are we inserting tweets by hand into IBM tone analyzer to get the json output?
+import sys
+sys.path.insert(0, '../../databases/cassandra/')
+from bigneuron_cassandradb_api import get_tweet_list, get_tweet_tones
 
 '''
     Renders the dashboard.
@@ -34,10 +39,13 @@ def home():
 '''
 @app.route("/{{ request.form['candidate'] }}", methods = ["GET", "POST"])
 def select_candidate():
+    retVal = None
     if request.method == "POST":
-
         radio = request.form['candidate'] #this retrieves which radio button was pressed
         session['candidate'] = radio
+        print "radio is -", radio
+        if radio == "trump":
+            radio = "realDonaldTrump"
         if radio == 'clinton':
             return render_template("pages/clinton/clinton.html", cand=radio)
         elif radio == 'cruz':
@@ -49,7 +57,8 @@ def select_candidate():
         elif radio == 'realDonaldTrump':
             return render_template("pages/trump/trump.html")
         else:
-            print "Error in select_candidate(). Need to make an error page"
+            retVal = "Error in select_candidate(). Need to make an error page"
+    return retVal
 
 '''
     Load visualizations from their respective candidate pages
@@ -57,7 +66,8 @@ def select_candidate():
 @app.route("/wordcloud", methods = ["GET", "POST"])
 def wordcloud():
     print "Inside wordcloud()"
-    cand = session['candidate']
+    retVal = None
+    cand   = session['candidate']
     print "In wordcloud, session['candidate'] is - ", cand
     if cand == 'clinton':
         return render_template("pages/clinton/clinton_wordcloud.html")
@@ -70,8 +80,10 @@ def wordcloud():
     elif cand == 'realDonaldTrump':
         return render_template("pages/trump/trump_wordcloud.html")
     else:
-        print "Error in wordcloud(). Need to make an error page"
-        
+        retVal = "Error in topic(). Need to make an error page"
+    return retVal
+
+
 '''
     Renders the realtime dashboard for any candidate
 '''
@@ -85,51 +97,49 @@ def realtime():
 '''
 @app.route('/network')
 def network():
-    print "Inside network() function" 
-    cand = session['candidate']
+    print "Inside network() function"
+    retVal = None
+    cand   = session['candidate']
     print "In Follower's Network, session['candidate'] is - ", cand
-    if cand == 'clinton':
-        return render_template("pages/clinton/clinton_network.html", cand=cand)
-    elif cand == 'cruz':
-        return render_template("pages/cruz/cruz_network.html", cand=cand)
-    elif cand == 'kasich':
-        return render_template("pages/kasich/kasich_network.html", cand=cand)
-    elif cand == 'sanders':
-        return render_template("pages/sanders/sanders_network.html", cand=cand)
-    elif cand == 'trump':
-        return render_template("pages/trump/trump_network.html", cand=cand)
+    if cand in ['clinton', 'cruz', 'kasich', 'sanders', 'trump']:
+        fpath = "pages/%s/%s_network.html" % (cand, cand)
+        retVal = render_template(fpath)
     else:
-        print "Error in network(). Need to make an error page"
+        retVal = "Error in topic(). Need to make an error page"
+    return retVal
+
 
 '''
     Renders the Topic Modelling visualization for any candidate
 '''
 @app.route('/topic')
 def topic():
-    print "Inside topic() function" 
-    cand = session['candidate']
+    print "Inside topic() function"
+    retVal = None
+    cand   = session['candidate']
     print "In Topic Modelling, session['candidate'] is - ", cand
-    if cand == 'clinton':
-        return render_template("pages/clinton/clinton_topicmodel.html", cand=cand)
-    elif cand == 'cruz':
-        return render_template("pages/cruz/cruz_topicmodel.html", cand=cand)
-    elif cand == 'kasich':
-        return render_template("pages/kasich/kasich_topicmodel.html", cand=cand)
-    elif cand == 'sanders':
-        return render_template("pages/sanders/sanders_topicmodel.html", cand=cand)
-    elif cand == 'trump':
-        return render_template("pages/trump/trump_topicmodel.html", cand=cand)
+    if cand in ['clinton', 'cruz', 'kasich', 'sanders', 'trump']:
+        fpath = "pages/%s/%s_topicmodel.html" % (cand, cand)
+        retVal = render_template(fpath)
     else:
-        print "Error in topic(). Need to make an error page"
+        retVal = "Error in topic(). Need to make an error page"
+    return retVal
 
+
+'''
+    Renders the Sentence-level SA page for candidate selected
+'''
 '''
     Renders the SA Sentence-level visualization for any candidate
 '''
 @app.route('/tweetlevel')
 def tweetlevel():
-    print "Inside tweet level function" 
+    print "Inside tweet level function"
     cand        = session['candidate']
-    tweet_num   = 3 #for testing; 
+    if cand == "trump":
+        cand = "realDonaldTrump"
+    print "CANDIDATE NAME IS --", cand
+    tweet_num = 20 #for testing;
     print "In Sentence-level, session['candidate'] is - ", cand
     if cand == 'HillaryClinton':
         tweet_list = get_tweets( cand, tweet_num )
@@ -144,17 +154,37 @@ def tweetlevel():
         tweet_list = get_tweets( cand, tweet_num )
         return render_template("pages/sanders/sanders_sa_sentence.html",tweet_list=tweet_list)
     elif cand == 'realDonaldTrump':
+        print "inside the trump section--", cand
         tweet_list = get_tweets( cand, tweet_num )
         return render_template("pages/trump/trump_sa_sentence.html",tweet_list=tweet_list)
     else:
         print "Error in tweetlevel(). Need to make an error page"
+
+
+'''
+    Ensures proper json files are stored for Sentence-level SA Amcharts chart per candidate tweet
+'''
+@app.route('/change_viz_by_id', methods=['GET','POST'])
+def change_viz_by_id():
+    retVal   = None
+    cand     = session['candidate']
+    if cand=="trump":
+        cand = "realDonaldTrump"
+    tweet_id = request.args['id'] #serial 1,2,3..
+    fpath    = "static/data/"
+    if cand in ['realDonaldTrump','clinton', 'cruz', 'kasich', 'sanders', 'trump']:
+        get_tweet_tones(cand, tweet_id, fpath)
+        retVal = ""
+    else:
+        retVal = "Error in tweetlevel()."
+    return retVal
 
 '''
     Renders the SA Document-level visualization for any candidate
 '''
 @app.route('/alltweet')
 def alltweet():
-    print "Inside All tweets - Document-level function" 
+    print "Inside All tweets function"
     cand = session['candidate']
     print "In Document-level, session['candidate'] is - ", cand
     if cand == 'clinton':
@@ -170,12 +200,31 @@ def alltweet():
     else:
         print "Error in alltweet(). Need to make an error page"
 
+
+
+############### Helper Functions #################
 '''
-    Renders the realtime dashboard for any candidate
+    Fetch tweet dictionary from db
+    Parameter(s): candidate (Type: string; Descr: Candidate name), tweet_num (Type: int; Descr: Number of tweets in dictionary, e.g. 20)
+    Return: List of Tuples, specifically [(tweet_id, tweet_text), ...]
 '''
-@app.route('/SA_PieChart_Multiple.html')
-def sa_piechart_multiple():
-    return render_template("viz/SA_PieChart_Multiple.html")
+def get_tweets( candidate, tweet_num ):
+    tweet_dict = get_tweet_list( candidate, tweet_num )
+    tweet_list = []
+    for key in tweet_dict:
+        print('KEY :: ', key)
+        print('TWEET DICT KEY :: ', tweet_dict[key])
+        tweet_list.append((key, tweet_dict[key]))
+    return tweet_list
+
+''' [THIS ONE MAY NOT BE NEEDED]
+    Fetch raw tweet json data from db
+    Parameter(s): candidate (Type: string; Descr: Candidate name), tweet_id (Type: string; Descr: Tweet identifier)
+    This will change likely --> Return: Tuple, specifically (tweet_id, tweet_text, ...)
+'''
+#def get_data( candidate, tweet_id ):
+#    return get_jsons( candidate, tweet_id )
+
 
 
 if __name__ == '__main__':
