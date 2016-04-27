7a512cee94ed01b614886cb546e0f25b21069d6a
pipa0979 <pipa0979@colorado.edu>
2016-04-27 12:56:46 -0600

    port number changed

--- WebApp/flask-app/flaskapp.py
+++ WebApp/flask-app/flaskapp.py
@@ -317,7 +317,7 @@ def get_tweets( candidate, tweet_num ):
 
 if __name__ == '__main__':
     
-    app.run(host='0.0.0.0', debug=True, port=5000, use_reloader=True)
+    app.run(host='0.0.0.0', debug=True, port=8000, use_reloader=True)
 
 
 
